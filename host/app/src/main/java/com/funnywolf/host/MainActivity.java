d707fce6e9ba8bba671f0a9e18a34b0f1d0695d6
zhaodongliang <zhaodongliang@zhihu.com>
2019-07-16 17:34:47 +0800

    使用插件内的资源

--- host/app/src/main/java/com/funnywolf/host/MainActivity.java
+++ host/app/src/main/java/com/funnywolf/host/MainActivity.java
@@ -1,13 +1,21 @@
 package com.funnywolf.host;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +26,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initViews() {
+        textView = findViewById(R.id.text_view);
+        imageView = findViewById(R.id.image_view);
         findViewById(R.id.load_plugin).setOnClickListener(v -> loadPlugin());
+        findViewById(R.id.start_plugin).setOnClickListener(v -> startPlugin());
     }
 
     private void loadPlugin() {
+        String pluginPath = getPluginPath();
+        if (pluginPath == null) {
+            Toast.makeText(this, "找不到插件", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageArchiveInfo(pluginPath, PackageManager.GET_ACTIVITIES);
+            AssetManager assetManager = AssetManager.class.newInstance();
+            AssetManager.class.getDeclaredMethod("addAssetPath", String.class)
+                    .invoke(assetManager, pluginPath);
+            Resources resources = new Resources(assetManager, getResources().getDisplayMetrics(), getResources().getConfiguration());
+            int textId = resources.getIdentifier("plugin_show_text", "string", packageInfo.packageName);
+            int imageId = resources.getIdentifier("plugin_image", "drawable", packageInfo.packageName);
+            textView.setText(resources.getText(textId));
+            imageView.setImageDrawable(resources.getDrawable(imageId, null));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startPlugin() {
         String pluginPath = getPluginPath();
         if (pluginPath == null) {
             Toast.makeText(this, "找不到插件", Toast.LENGTH_SHORT).show();
